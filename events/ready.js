6681239aaf9abedb8d4ef35bc1fefd441eff69fb
Daan Houbrechts <patattendaan@gmail.com>
2021-06-18 22:40:00 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`);
-    bot.user.setActivity("🎶 KVK", {type: "LISTENING"});
+    bot.user.setActivity("🎶 kinderen voor kinderen", {type: "LISTENING"});
 }
